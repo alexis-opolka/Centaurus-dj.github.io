086fb05e93dd91615603bb6045b4e89b4ae63d75
Centaurus-dj <53085471+Centaurus-dj@users.noreply.github.com>
2020-08-16 23:53:53 +0200

    I added files and I started developing a feature for accessing website, for an educational or commercial purpose, uploaded on the repository without having to add a public link on each page and clickable by everyone going on the site

--- js/funcs.js
+++ js/funcs.js
@@ -7,5 +7,57 @@ function ShowDropDowns(dropdownid, indicid){
     } else {
       indic.innerHTML = "-";
     }
+  };
+
+function AccessSitesForm(){
+  if(window.accessasked === true){
+    alert("You can't call twice the process, wait a little longer before the form appear");
+  }else {
+    window.accessasked = true;
+    const body = document.createElement("div");
+    const formTitle = document.createElement("div");
+    const formDirectText = document.createElement("p");
+    const formDirectEntry = document.createElement("input");
+    const formGuestText = document.createElement("p");
+    const formGuestDiv = document.createElement("div");
+    const formGuestTokenTxt = document.createElement("p");
+    const formGuestTokenEntry = document.createElement("input");
+
+    document.getElementById("main-page").appendChild(body);
+    body.appendChild(formTitle);
+    body.appendChild(formDirectText);
+    body.appendChild(formDirectEntry);
+    body.appendChild(formGuestText);
+    body.appendChild(formGuestDiv);
+    formGuestDiv.appendChild(formGuestTokenTxt);
+    formGuestDiv.appendChild(formGuestTokenEntry);
+
+    body.setAttribute("class", "SitesForm dark-grey-bg white-text little-mid-text");
+    formTitle.setAttribute("class", "center-text mid-dark-orange-text")
+
+    formTitle.innerHTML = "Méthodes d'accès";
+    formDirectText.textContent = "Accès direct, via nom du site :";
+    formGuestText.textContent = "Accès via token :";
+    formGuestTokenTxt.textContent = "Veuillez saisir votre token :";
+    
   }
-  
+};
+
+
+
+
+function TestAccessSites(){
+  document.write('<div id=\"cache\">Veuillez patienter...<\/div>');
+    document.write('<script type=\"text\/javascript\">');
+    var nava = (document.layers);
+    var dom = (document.getElementById);
+    var iex = (document.all);
+    if (nava) { cach = document.cache }
+    else if (dom) { cach = document.getElementById("cache").style }
+    else if (iex) { cach = cache.style }
+    largeur = screen.width;
+    cach.visibility = "visible";
+    document.write('<\/script>');
+}
+
+accessasked = false;
