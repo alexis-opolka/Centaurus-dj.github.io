8bedfa0d719d79780fd35fd756f0838659cffe5d
Centaurus <alexis.opolka@protonmail.com>
2022-04-10 22:04:52 +0200

    Add 'matured' state of homepage + footer

--- new/js/libs/themeteor/main.js
+++ new/js/libs/themeteor/main.js
@@ -1,5 +1,5 @@
 export function whut() {
-  console.warning('Warning, my friend.')
+  console.warning('Warning, my friend.');
 }
 
 export class MainClass {
@@ -15,7 +15,7 @@ export class MainClass {
       this.CSSThemeData = {
         'dark': 'background-color: #1e1e1e; color: #ffffff;',
         'light': 'background-color: #c3c3c3; color: #000000;'
-      }
+      };
 
       this.config = ''; // Here is added the configuration of the page
       this.CSSconfig = ''; // Here is added the configuration of the page in CSS code
@@ -54,31 +54,31 @@ export class MainClass {
       };
       return themeOuput;
     }
-  };
+  }
 
   build() {
     document.body.classList = this.config;
     document.body.style = '';
-    console.log('Built -> body-class-data: ' + this.config + '.')
+    console.log('Built -> body-class-data: ' + this.config + '.');
     this.footerObject.build();
-    console.log('Built -> footer-class-data: ' + this.footerObject.cssData + '.')
-  };
+    console.log('Built -> footer-class-data: ' + this.footerObject.cssData + '.');
+  }
   buildCSS() {
     document.body.classList = '';
     document.body.style = this.CSSconfig;
-    console.log('Built -> body-style-data: ' + this.CSSconfig + '.')
+    console.log('Built -> body-style-data: ' + this.CSSconfig + '.');
     this.footerObject.buildCSS();
-    console.log('Built -> footer-style-data: ' + this.footerObject.cssData + ".")
+    console.log('Built -> footer-style-data: ' + this.footerObject.cssData + ".");
   }
 
   setThemeThenBuild(themeName) {
     this.setTheme(themeName);
     this.build();
-  };
+  }
   setThemeThenCSSBuild(themeName) {
     this.setTheme(themeName);
     this.buildCSS();
-  };
+  }
   getCurrentThemeName(){
     return this.currentTheme;
   }
@@ -92,10 +92,12 @@ class FooterClass {
     this.copyrightDate = "2020-2022";
     this.copyrightHolder = "Alexis Opolka (Centaurus)";
     this.rightsHeld = "All Rights Reserved";
-    this.homePage = "https://centaurus-dj.github.io/new/";
-    this.aboutPage = "https://centaurus-dj.github.io/new/about/";
-    this.contactPage = "https://centaurus-dj.github.io/new/contact";
-    this.privacyPolicyPage = "https://centaurus-dj.github.io/new/privacy-policy/";
+
+    this.findMeHref = "/new/me/#cloud-repositories";
+    this.homePageHref = "/new/";
+    this.aboutPageHref = "/new/about/";
+    this.contactPageHref = "/new/contact";
+    this.privacyPolicyPageHref = "/new/privacy-policy/";
 
     this.footerId = footerId;
     this.footerDiv = document.getElementById(this.footerId);
@@ -106,14 +108,87 @@ class FooterClass {
 
   constructFooter() {
     // Legal data
-    legalData = document.createElement('div');
+    var linkCopyrightHolder = document.createElement('a');
+    linkCopyrightHolder.href = this.findMeHref;
+    linkCopyrightHolder.textContent = this.copyrightHolder;
+
+    var legalData = document.createElement('div');
     legalData.id = "legal-data";
-    legalData.
+
+    var loveImg = document.createElement('img');
+    loveImg.src = "/new/src/png/heart.png";
+    loveImg.alt = "Love";
+    loveImg.style = "width: 20px; height: 20px;";
+
+    var hostData = document.createElement('a');
+    hostData.href = "https://github.com";
+    hostData.textContent = "GitHub";
+    hostData.target = "_blank";
+
+    var hostingData = document.createElement('div');
+    hostingData.id = "hosting-data";
+    hostingData.innerHTML = `Hosted with ${loveImg.outerHTML} by ${hostData.outerHTML}`;
+
+    var copyrightData = document.createElement('div');
+    copyrightData.id = "copyright-data";
+    copyrightData.innerHTML = `Copyright &copy; ${this.copyrightDate} ${linkCopyrightHolder.outerHTML} - ${this.rightsHeld}`;
+
+    legalData.appendChild(hostingData);
+    legalData.appendChild(copyrightData);
+
+    // Footer links
+    var linksHolder = document.createElement('div');
+    linksHolder.id = "footer-links-holder";
+    linksHolder.className = "footer-wrap";
+
+    var linksLeftSubholder = document.createElement('div');
+    linksLeftSubholder.id = "footer-links-left-subholder";
+    linksLeftSubholder.className = "footer-subwrap";
+
+    var linksRightSubHolder = document.createElement('div');
+    linksRightSubHolder.id = "footer-links-right-subholder";
+    linksRightSubHolder.className = "footer-subwrap";
+
+    var linkHomepage = document.createElement('div');
+    var anchorLinkHomepage = document.createElement('a');
+    anchorLinkHomepage.href = this.homePageHref;
+    anchorLinkHomepage.textContent = "Home Page";
+    linkHomepage.appendChild(anchorLinkHomepage);
+
+    var linkAbout = document.createElement('div');
+    var anchorLinkAbout = document.createElement('a');
+    anchorLinkAbout.href = this.aboutPageHref;
+    anchorLinkAbout.textContent = "About";
+    linkAbout.appendChild(anchorLinkAbout);
+
+    var linkContact = document.createElement('div');
+    var anchorLinkContact = document.createElement('a');
+    anchorLinkContact.href = this.contactPageHref;
+    anchorLinkContact.textContent = "Contact me";
+    linkContact.appendChild(anchorLinkContact);
+
+    var linkPrivacy = document.createElement('div');
+    var anchorLinkPrivacy = document.createElement('a');
+    anchorLinkPrivacy.href = this.privacyPolicyPageHref;
+    anchorLinkPrivacy.textContent = "Privacy Policy";
+    linkPrivacy.appendChild(anchorLinkPrivacy);
+
+    // Append the childs
+    this.footerDiv.appendChild(linksHolder);
+
+    linksHolder.appendChild(linksLeftSubholder);
+    linksLeftSubholder.appendChild(linkHomepage);
+    linksLeftSubholder.appendChild(linkContact);
+
+    linksHolder.appendChild(linksRightSubHolder);
+    linksRightSubHolder.appendChild(linkAbout);
+    linksRightSubHolder.appendChild(linkPrivacy);
+
     this.footerDiv.appendChild(legalData);
   }
 
   applyTheme(themeData) {
-    console.log(themeData)
+    console.log(themeData);
     this.themeName = themeData.name;
     this.footerDiv.classList = themeData.classData;
   }
